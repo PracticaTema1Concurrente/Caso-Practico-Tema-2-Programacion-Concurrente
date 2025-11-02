16c99823b71c942337e423401e96731550a26e43
gabriel140282 <gkaakmor@myuax.com>
2025-11-02 17:56:49 +0100

    V0.1

---
+++ src/test/java/com/starkindustries/casopracticotema2programacionconcurrente/CasoPracticoTema2ProgramacionConcurrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starkindustries.casopracticotema2programacionconcurrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPracticoTema2ProgramacionConcurrenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
